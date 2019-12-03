e6b07dc32cf57b309df29017d0e85d92d4648a7d
jakekgrogan <jake.kgrogan@gmail.com>
2019-12-03 21:28:24 +0000

    Added openScope and getSymbol

--- assignment2/src/SymbolTable.java
+++ assignment2/src/SymbolTable.java
@@ -1,15 +1,17 @@
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Stack;
 import java.util.Hashtable;
+import java.util.Arrays;
 
 public class SymbolTable
 {
 
 
-    private final SymbolTableEntry marker = new SymbolTableEntry("MRKR", "MRKR", "MRKR");
+    final String marker = "";
 
     Hashtable<Integer, LinkedList<SymbolTableEntry>> hashTable;
-    Stack<SymbolTableEntry> undoStack;
+    Stack<String> undoStack;
 
     public SymbolTable() {
         this.hashTable = new Hashtable<>();
@@ -18,20 +20,40 @@ public class SymbolTable
         this.undoStack.push(marker);
     }
 
-    public void getSymbol(String id) {
+    public Object getSymbol(String id) {
         Integer key = id.hashCode();
         LinkedList bucket = hashTable.get(key);
+        
+        Object obj;
+
+        // Search the bucket for the symbol entry
+        ListIterator listIter = bucket.listIterator();
+        while (listIter.hasNext()) {
+            SymbolTableEntry smbl = (SymbolTableEntry)listIter.next();
+            if (smbl.id.equals(id)) {
+                obj = smbl;
+                return obj;
+            }
+        }
+        obj = "NOTFOUND";
+        return obj;      
     }
 
     public void putSymbol() {
-
+        //
     }
 
     public void openScope() {
-        //
+        undoStack.add(marker);
     }
 
     public void closeScope() {
-        //
+        ;
+    }
+
+    public void printStack() {
+        System.out.println("---------PRINTING UNDO STACK---------");
+        System.out.println(Arrays.toString(this.undoStack.toArray()));
+        System.out.println("-------------------------------------");
     }
 }
